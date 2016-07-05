ca999e10fb1e719235801bd8c07be6293c19ef0f
Chris Yallop <chris.yallop@adnams.co.uk>
2016-07-05 14:20:25 +0100

    Multiples of five now print Buzz

--- fizzBuzz.py
+++ fizzBuzz.py
@@ -3,4 +3,8 @@ class FizzBuzz:
      fb = range(1, 101)
      for i in range(2,101,3):
          fb[i] = 'Fizz'
+
+     for i in range(4,101,5):
+         fb[i] = 'Buzz'
+
      return fb
--- fizzBuzzTest.py
+++ fizzBuzzTest.py
@@ -6,17 +6,23 @@ class FizzBuzzTest(unittest.TestCase):
         self.fizzBuzz = FizzBuzz()
 
     def test_100_entries_are_printed(self):
-        sequence = self.fizzBuzz.generate()
-        self.assertEqual(100, len(sequence))
-        self.assertEqual(1, sequence[0])
-        self.assertEqual(100, sequence[-1])
+        fb = self.fizzBuzz.generate()
+        self.assertEqual(100, len(fb))
+        self.assertEqual(1, fb[0])
+        self.assertEqual(98, fb[-3])
 
     def test_multiples_of_three_print_fizz(self):
-        sequence = self.fizzBuzz.generate()
-        self.assertEqual('Fizz', sequence[2])
-        self.assertEqual('Fizz', sequence[5])
+        fb = self.fizzBuzz.generate()
+
         for i in range(2,101,3):
-            sequence[i] = 'Fizz'
+            if (i+1) % 5 != 0:
+                self.assertEqual('Fizz', fb[i])
+
+    def test_multiples_of_five_print_buzz(self):
+        fb = self.fizzBuzz.generate()
+
+        for i in range(4,101,5):
+            self.assertEqual('Buzz', fb[i])
 
 if __name__ == '__main__':
     unittest.main()
